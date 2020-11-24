f2c922615e5332fecbf00b246db18e5f9013a60d
GDhnnys <dhenys.1002@gmail.com>
2020-11-24 18:46:47 +0700

    Upload BAB 04
    Basic Data Type Program

---
+++ BAB 04/4.1ProgramTipeDataDasar.py
@@ -0,0 +1,19 @@
+# Start Program
+umur = 53
+phi = 3.14
+nama = "Yuniar Supardi"
+penulis = True
+profesi_komputer = ["System Analyst", "Programmer", "Operator"]
+print(type(umur))
+print("umur : ",umur)
+print(type(phi))
+print("phi: ",phi)
+print(type(nama))
+print("nama", nama)
+print(type(penulis))
+print("Profesi Penulis : ", penulis)
+print(type(profesi_komputer))
+print("Profesi Komputer - 0", profesi_komputer[0])
+print("Profesi Komputer - 1", profesi_komputer[1])
+print("Profesi Komputer - 2", profesi_komputer[2])
+# End Program
