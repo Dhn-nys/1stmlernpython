0043186f67b7e178e08ac91ca9b2f6c716508a16
デニス <dhenys.1002@gmail.com>
2021-02-26 05:12:10 +0700

    Update 2.1.1ContohProgramSekuensial.py
    Rewrite ^.^

--- BAB 02/2.1.1ContohProgramSekuensial.py
+++ BAB 02/2.1.1ContohProgramSekuensial.py
@@ -1,8 +1,10 @@
 # BAB 2
+# Hal. 6
 
 # Start program
 panjang=int(input("Panjang ?")) # Show input value panjang
 lebar=int(input("Lebar ?")) # Show input value lebar
 luas=panjang*lebar # Formula luas 
-print("Luas =",luas) #Show output value luas
+print("Luas =",luas) 
+# Show output value luas ("Luas = *Value*")
 # End program
