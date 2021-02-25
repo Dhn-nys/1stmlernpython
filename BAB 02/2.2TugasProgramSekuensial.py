460ed61286f3925fd6a91f0299ed02cfd1d8f2b2
デニス <dhenys.1002@gmail.com>
2021-02-26 05:13:44 +0700

    Update 2.2TugasProgramSekuensial.py
    Rewrite

--- BAB 02/2.2TugasProgramSekuensial.py
+++ BAB 02/2.2TugasProgramSekuensial.py
@@ -1,8 +1,10 @@
 # BAB 2
+# Hal. 8
 
 # Start program
 QTY=int(input("Banyak Barang ?")) # Input value Banyak barang.
 HRGBR=int(input("Harga Barang ?")) # Input value Harga barang.
 JMLBYR=QTY*HRGBR # Formula the Jumlah Bayar.
-print ("Jumlah Bayar = ", JMLBYR) # Output value Jumlah bayar.
+print ("Jumlah Bayar = ", JMLBYR) 
+# Output value Jumlah bayar. ("Jumlah Bayar = *Value*")
 # End program
